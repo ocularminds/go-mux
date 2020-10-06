0e40e1bc620ef16c224712fbf57be3c5dfaad4ad
Festus B. Jejelowo <mail.festus@gmail.com>
2020-10-06 23:11:39 +0100

    All implementations

--- docker-postgres.bat
+++ docker-postgres.bat
@@ -1,1 +1,5 @@
-start-docker && docker run -it -p 5432:5432 -d postgres
+//frontend channels
+//Web, iOS, Android, Alexa, Sonos, Cortana, Google Home, Chromecast, Roku, and FireTV
+
+https://ordina-jworks.github.io/architecture/2017/12/22/Tech-Lead.html
+https://blog.coleadership.com/what-does-it-take-to-be-a-successful-tech-lead/
--- mux/service.go
+++ mux/service.go
@@ -2,21 +2,58 @@ package mux
 
 import (
 	"database/sql"
-	"errors"
 )
 
 func (p *Product) getProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	return db.QueryRow("SELECT name, price FROM products WHERE id=$1",
+		p.ID).Scan(&p.Name, &p.Price)
 }
 
-func (p *Product) getProducts(db *sql.DB) ([]Product, error) {
-	return nil, errors.New("Not implemented")
+func (p *Product) updateProduct(db *sql.DB) error {
+	_, err :=
+		db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3",
+			p.Name, p.Price, p.ID)
+
+	return err
 }
 
 func (p *Product) deleteProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
+
+	return err
 }
 
 func (p *Product) createProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	err := db.QueryRow(
+		"INSERT INTO products(name, price) VALUES($1, $2) RETURNING id",
+		p.Name, p.Price).Scan(&p.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getProducts(db *sql.DB, start, count int) ([]Product, error) {
+	rows, err := db.Query(
+		"SELECT id, name,  price FROM products LIMIT $1 OFFSET $2",
+		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	products := []Product{}
+
+	for rows.Next() {
+		var p Product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
 }
